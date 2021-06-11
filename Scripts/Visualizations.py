bf788b71d977ac707ee1f919b0dfcb9af37c6fc9
NickJeurissen <n.h.j.jeurissen@student.tue.nl>
2021-06-11 11:51:28 +0200

    Added new node link clustering algorithm
    Node link uses graph.json to load variables. We still need to initiate this somehow when the user uploads the data to the server

--- Scripts/Visualizations.py
+++ Scripts/Visualizations.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3.8
 
-import numpy as np
+
 #The python file for all the functions for the different visualizations
 import pandas as pd
+import numpy as np
+import json as json
 
 async def makeGraphs(data):
     data_0 = await extract_column(data, 0)
@@ -10,12 +12,39 @@ async def makeGraphs(data):
     dict_mail_traffic = await dict_mail_traffic_calculate(data_0)
     return await calculate_values_per_year(unique_years, dict_mail_traffic)
 
+
+async def writeJSON(data):  # This function is used to create graph.json only has to be done once
+    nodes = []
+    links = []
+    id_sender_list = [item[1] for item in data]
+    id_receiver_list = [item[4] for item in data]
+    id_list = id_sender_list + id_receiver_list
+    unique_id_list = []
+    for x in id_list:
+        if x not in unique_id_list:
+            unique_id_list.append(x)
+    for i in unique_id_list:
+        id = i
+        nodes.append({"id": id})
+
+    for i in range(len(data)):
+        id = "placeholder"
+        source = data[i][1]
+        target = data[i][4]
+        weight = 1
+        links.append({"id": id, "source": source, "target": target, "weight": weight})
+    jsonstring = {"nodes": nodes, "links": links}
+    with open('templates/graph.json', 'w') as outfile:
+        json.dump(jsonstring, outfile, indent=4)
+
 async def forceDirectedGraph(data):
     list_from_emails = await extract_column(data, 1)
     list_to_emails = await extract_column(data, 4)
     list_of_jobs = await extract_column(data, 3)
     numbers, uniques = pd.factorize(list_of_jobs, sort=True)
     return [list_from_emails, list_to_emails, numbers.tolist()]
+
+
 
 def makeMatrix(data):
     #We have to first find how many ID's we have in the file
