e40ff9053a5c1f90d0a51e708ba51f4652bd3029
NickJeurissen <n.h.j.jeurissen@student.tue.nl>
2021-06-25 11:50:03 +0200

    Changes to NodeLink
    Added colour and legend to nodelink graph

--- Scripts/scratch.py
+++ Scripts/scratch.py
@@ -1,13 +1,41 @@
 import pandas as pd
 import numpy as np
+from Visualizations import *
+import csv as csv
+import json
 
 pdData = pd.read_csv("csv_files/enron-v1.csv")
 data = pdData.to_numpy()
-list_of_jobs = [item[3] for item in data]
-numbers, uniques = pd.factorize(list_of_jobs, sort=True)
-numbers = numbers.tolist()
-for i in range(len(numbers)):
+nodes = []
+links = []
+id_sender_list = [item[1] for item in data]
+id_receiver_list = [item[4] for item in data]
+id_list = id_sender_list + id_receiver_list
+unique_id_list = []
+for x in id_list:
+    if x not in unique_id_list:
+        unique_id_list.append(x)
+for i in unique_id_list:
+    id = i
+    nodes.append({"id": id})
 
-    print(numbers[i])
-    print(list_of_jobs[i])
-    print()
+for i in range(len(data)):
+    id = "placeholder"
+    source = data[i][1]
+    target = data[i][4]
+    weight = 4
+    links.append({"id": id, "source": source, "target": target, "weight": weight})
+jsonstring = {"nodes": nodes, "links": links}
+with open('templates/graph.json', 'w') as outfile:
+    json.dump(jsonstring, outfile, indent=4)
+
+data = {}
+with open("csv_files/enron-v1.csv") as csvFile:
+    csvReader = csv.DictReader(csvFile)
+    for rows in csvReader:
+        id = rows["date"]
+        data[id] = rows
+
+with open('templates/enron-v1.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4)
+
--- Scripts/templates/NodeLink.js
+++ Scripts/templates/NodeLink.js
@@ -10,8 +10,8 @@
         ];*/
 
     if (NodeData !== []) { //function will start when data is present
-        for (i = 0; i < NodeData[0].length/10; i++) { //NodeData[0].length takes all info but that makes a graph with MANY nodes
-            links.push({source: NodeData[0][i], target: NodeData[1][i]})
+        for (i = 0; i < NodeData[0].length/20; i++) { //NodeData[0].length takes all info but that makes a graph with MANY nodes
+            links.push({source: NodeData[0][i], target: NodeData[1][i], status: "black"})
             if (NodeData[2][i] === 0) {job_list.push("CEO")}
             if (NodeData[2][i] === 1) {job_list.push("Director")}
             if (NodeData[2][i] === 2) {job_list.push("Employee")}
@@ -20,7 +20,7 @@
             if (NodeData[2][i] === 5) {job_list.push("Managing Director")}
             if (NodeData[2][i] === 6) {job_list.push("President")}
             if (NodeData[2][i] === 7) {job_list.push("Trader")}
-            if (NodeData[2][i] === 8) {job_list.push("Unkown")}
+            if (NodeData[2][i] === 8) {job_list.push("Unknown")}
             if (NodeData[2][i] === 9) {job_list.push("Vice President")}
 
         }
@@ -28,18 +28,89 @@
 
         // create empty nodes array
         var nodes = {};
+        var nodeToStatus = {};
         // compute nodes from links data
         links.forEach(function (link) {
-            link.source = nodes[link.source] ||
-                (nodes[link.source] = {name: link.source});
-            link.target = nodes[link.target] ||
-                (nodes[link.target] = {name: link.target});
+            link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+            link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+            nodeToStatus[link.source.name] = link.status;
+            nodeToStatus[link.target.name] = link.status;
         });
+        function nodeColor(d) {
+            if (job_list !== [])
+                if (job_list[d.index] === "CEO") {
+                    return 'blue';
+                }
+                if (job_list[d.index] === "Director") {
+                    return 'green';
+                }
+                if (job_list[d.index] === "Employee") {
+                    return 'red';
+                }
+                if (job_list[d.index] === "In House Lawyer") {
+                    return 'yellow';
+                }
+                if (job_list[d.index] === "Manager") {
+                    return 'black';
+                }
+                if (job_list[d.index] === "Managing Director") {
+                    return 'white';
+                }
+                if (job_list[d.index] === "President") {
+                    return 'cyan';
+                }
+                if (job_list[d.index] === "Trader") {
+                    return 'brown';
+                }
+                if (job_list[d.index] === "Unknown") {
+                    return 'pink';
+                }
+                if (job_list[d.index] === "Vice President") {
+                    return 'purple';
+                }
+            else {
+                return 'black';
+            }
+        }
 
         // add a SVG to the body for our viz
         var nodeLink = d3.select('body').append('svg')
             .attr('width', width)
             .attr('height', height);
+
+        var legendx = 450;
+        var legendy = 100;
+
+        nodeLink.append('rect')
+          .attr('x', 640)
+          .attr('y', 215)
+          .attr('width', 160)
+          .attr('height', 300)
+          .attr('stroke', 'black')
+          .attr('fill', '#69a3b2');
+
+        // variables on the nodelink graph
+        nodeLink.append("circle").attr("cx",200 + legendx).attr("cy",130 + legendy).attr("r", 6).style("fill", "blue")
+        nodeLink.append("circle").attr("cx",200 + legendx).attr("cy",160 + legendy).attr("r", 6).style("fill", "green")
+        nodeLink.append("circle").attr("cx",200 + legendx).attr("cy",190 + legendy).attr("r", 6).style("fill", "red")
+        nodeLink.append("circle").attr("cx",200 + legendx).attr("cy",220 + legendy).attr("r", 6).style("fill", "yellow")
+        nodeLink.append("circle").attr("cx",200 + legendx).attr("cy",250 + legendy).attr("r", 6).style("fill", "black")
+        nodeLink.append("circle").attr("cx",200 + legendx).attr("cy",280 + legendy) .attr("r", 6).style("fill", "white")
+        nodeLink.append("circle").attr("cx",200 + legendx).attr("cy",310 + legendy).attr("r", 6).style("fill", "cyan")
+        nodeLink.append("circle").attr("cx",200 + legendx).attr("cy",340 + legendy).attr("r", 6).style("fill", "brown")
+        nodeLink.append("circle").attr("cx",200 + legendx).attr("cy",370 + legendy).attr("r", 6).style("fill", "pink")
+        nodeLink.append("circle").attr("cx",200 + legendx).attr("cy",400 + legendy).attr("r", 6).style("fill", "purple")
+        nodeLink.append("text").attr("x", 220 + legendx).attr("y", 130 + legendy).text("CEO").style("font-size", "15px").attr("alignment-baseline","middle")
+        nodeLink.append("text").attr("x", 220 + legendx).attr("y", 160 + legendy).text("Director").style("font-size", "15px").attr("alignment-baseline","middle")
+        nodeLink.append("text").attr("x", 220 + legendx).attr("y", 190 + legendy).text("Employee").style("font-size", "15px").attr("alignment-baseline","middle")
+        nodeLink.append("text").attr("x", 220 + legendx).attr("y", 220 + legendy).text("In House Lawyer").style("font-size", "15px").attr("alignment-baseline","middle")
+        nodeLink.append("text").attr("x", 220 + legendx).attr("y", 250 + legendy).text("Manager").style("font-size", "15px").attr("alignment-baseline","middle")
+        nodeLink.append("text").attr("x", 220 + legendx).attr("y", 280 + legendy).text("Managing Director").style("font-size", "15px").attr("alignment-baseline","middle")
+        nodeLink.append("text").attr("x", 220 + legendx).attr("y", 310 + legendy).text("President").style("font-size", "15px").attr("alignment-baseline","middle")
+        nodeLink.append("text").attr("x", 220 + legendx).attr("y", 340 + legendy).text("Trader").style("font-size", "15px").attr("alignment-baseline","middle")
+        nodeLink.append("text").attr("x", 220 + legendx).attr("y", 370 + legendy).text("Unknown").style("font-size", "15px").attr("alignment-baseline","middle")
+        nodeLink.append("text").attr("x", 220 + legendx).attr("y", 400 + legendy).text("Vice President").style("font-size", "15px").attr("alignment-baseline","middle")
+
 
 
         // use the force
@@ -68,7 +139,11 @@ var job = {};
             .enter().append('circle')
             .attr('class', 'node')
             .attr('r', 5) //radius of circle
-            .attr("fill", "red")
+            .style("fill", nodeColor)
+            .style("stroke", "black")
+            .style("opacity", 1);
+
+
                 // for(var i=0;i<list_of_jobs.length; i++){
                 //     job_color[i] = np.random.choice(range(256), size=3)}
                 //
@@ -87,19 +162,25 @@ var job = {};
         .style('border-radius', '5px')
         .style('opacity', '0')
 
-    nodeLink.selectAll(".node").on("mouseover", function (d,i){
+    nodeLink.selectAll(".node").on("mouseover", function (d,i) {
         tooltip.style("opacity", 1)
 
-         .html("Job: " + job_list[i])
-
-         .style("left", (d3.event.pageX) + "px")
-         .style("top", (d3.event.pageY) + "px")
-        console.log(NodeData[2][i])
-    });
 
 
 
-           
+
+
+                .html("Job: " + job_list[i])
+
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px")
+            console.log(d)
+        });
+
+    // nodeLink.select(this).on("click", function(d, i) {
+    //     tooltip.style("opacity", 0)
+    //     this("opacity", 0.3)
+    // });
 
 
         function tick(e) {
