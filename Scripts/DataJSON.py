bbe555630fd45c0fcb706b98aa6e3df34fce3ab2
SvenMokveldje <39305538+SvenMokveldje@users.noreply.github.com>
2021-06-30 14:49:51 +0200

    Make a complete json file

--- Scripts/DataJSON.py
+++ Scripts/DataJSON.py
@@ -6,22 +6,39 @@ import json
 import csv
 
 def makeJSONFile(data):
-
     JsonData = {}
     JsonData['data'] = []
 
-
     for i in range(len(data)):
+        if not JsonData['data']:
             JsonData['data'].append({
                 'From-ID': data[i][1],
                 'To-ID': data[i][4],
                 'From-Email': data[i][2],
-                 'To-Email': data[i][5],
+                'To-Email': data[i][5],
                 'From-Job': data[i][3],
                 'To-Job': data[i][6],
+                'Amount-Links': 0
             })
+        else:
+            foundlink = False
+            for d in JsonData['data']:
+                if d['From-ID'] == data[i][1] and d['To-ID'] == data[i][4]:
+                    foundlink = True
+                    d['Amount-Links'] += 1
+                    break
 
+            if not foundlink:
+                JsonData['data'].append({
+                    'From-ID': data[i][1],
+                    'To-ID': data[i][4],
+                    'From-Email': data[i][2],
+                    'To-Email': data[i][5],
+                    'From-Job': data[i][3],
+                    'To-Job': data[i][6],
+                    'Amount-Links': 0
+                })
 
+    return JsonData
 
-    return  JsonData
 
